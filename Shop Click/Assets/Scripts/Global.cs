0807532699e0f4030f49dfa365773cd251fb8f1f
atet <atet.kao@gmail.com>
2019-08-18 21:37:55 -0700

    UI first draft with Shop screen mockup.

---
+++ Shop Click/Assets/Scripts/Global.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour
+{
+    private int levelCurrent;
+    private int experienceCurrent;
+    private int goldCurrent;
+    private int goldLifetime;
+    private int goldSpent;
+    private int gemsCurrent;
+    private int gemsLifetime;
+    private int gemsSpent;
+    private int chakraCurrent;
+    private int chakraLifetime;
+    private int chakraSpent;
+}
