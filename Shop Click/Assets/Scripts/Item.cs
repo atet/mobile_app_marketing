5ddd8cd2f4f2152d2246f92f48e68219b92b190b
atet <atet.kao@gmail.com>
2019-08-19 17:15:48 -0700

    Test inventory system.

---
+++ Shop Click/Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private string imagePath;
+    private string nameDisplay;
+    private ulong value;
+    private int level;
+
+    public Item()
+    {
+        
+    }
+}
