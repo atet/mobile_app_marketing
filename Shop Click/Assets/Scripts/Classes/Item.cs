7d60e23ff502ec828d8af5df4d2226ef94d0a350
atet <atet.kao@gmail.com>
2019-08-20 18:23:34 -0700

    Template for Item class.

--- Shop Click/Assets/Scripts/Classes/Item.cs
+++ Shop Click/Assets/Scripts/Classes/Item.cs
@@ -4,10 +4,57 @@ using UnityEngine;
 
 public class Item
 {
-    private string imagePath;
-    private string nameDisplay;
+    private string name;
+    private string type;
+    private string prerequsiteUnlock;
+    private string prerequsiteUnlockCostResearchScrolls;
+    private int tier;
     private ulong value;
-    private int level;
+    private int timeCrafting;
+    private ulong xPMerchant;
+    private ulong xPWorker;
+    private string prerequisiteWorker1;
+    private int prerequisiteWorker1Level;
+    private string prerequisiteWorker2;
+    private int prerequisiteWorker2Level;
+    private ulong costIron;
+    private ulong costWood;
+    private ulong costHide;
+    private ulong costHerbs;
+    private ulong costSteel;
+    private ulong costTitanium;
+    private ulong costElectricity;
+    private ulong costOil;
+    private string costComponent1Name;
+    private string costComponent1Quality;
+    private ulong costComponent1;
+    private string costComponent2Name;
+    private string costComponent2Quality;
+    private ulong costComponent2;
+    private int statATK;
+    private int statDEF;
+    private int statHP;
+    private string upgradeCrafting1;
+    private int upgradeCrafting1Count;
+    private string upgradeCrafting2;
+    private int upgradeCrafting2Count;
+    private string upgradeCrafting3;
+    private int upgradeCrafting3Count;
+    private string upgradeCrafting4;
+    private int upgradeCrafting4Count;
+    private string upgradeCrafting5;
+    private int upgradeCrafting5Count;
+    private string upgradeAscension1;
+    private int upgradeAscension1Cost;
+    private string upgradeAscension2;
+    private int upgradeAscension2Cost;
+    private string upgradeAscension3;
+    private int upgradeAscension3Cost;
+    private int energyDiscount;
+    private int energySurcharge;
+    private int energySuggest;
+    private int energySpeedUp;
+
 
     public Item()
     {
